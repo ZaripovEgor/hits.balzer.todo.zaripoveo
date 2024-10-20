0d4ce8feadaec5567e123b361403873477235242
ZaripovEgor <zaripov-egor@mail.ru>
2024-10-20 17:18:46 +0500

    Task1

---
+++ TodoServerApp/Data/TaskItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using System.Runtime.CompilerServices;
+
+namespace TodoServerApp.Data
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; } 
+        public string? Description { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? FinishDate { get; set; }
+    }
+}
